ad14ba3e0cb63913b77cbd21ebd5949a9f388161
Despicable-at <agyemangtakyi2021@gmail.com>
2025-04-19 18:27:48 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
+import { NotificationProvider } from './NotificationContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
