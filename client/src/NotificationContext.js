0674c3f5442fcbb382ebda83b141941977f29d80
Despicable-at <agyemangtakyi2021@gmail.com>
2025-04-19 17:11:35 +0000

    Update NotificationContext.js

--- client/src/NotificationContext.js
+++ client/src/NotificationContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import Notification from '../Notification';
+import Notification from './Notification';
 
 const NotificationContext = createContext();
 
