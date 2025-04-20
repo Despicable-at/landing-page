be5d37ef204a34bb49d4bd0f13e1e0c162b2b300
Despicable-at <agyemangtakyi2021@gmail.com>
2025-04-20 20:37:25 +0000

    Update OAuthCallback.js

--- client/src/OAuthCallback.js
+++ client/src/OAuthCallback.js
@@ -7,8 +7,19 @@ const OAuthCallback = ({ setLoadingScreen }) => {  // Add setLoadingScreen prop
   const location = useLocation();
   const showNotification = useNotification();
 
-  useEffect(() => {
-    setLoadingScreen(true); // Activate loading screen immediately
+useEffect(() => {
+  setLoadingScreen(true);
+  
+  const hashParams = new URLSearchParams(location.hash.split("?")[1]);
+  const token = hashParams.get('token');
+
+  if (token) {
+    localStorage.setItem('token', token);
+    window.dispatchEvent(new Event('storage')); // Force storage event
+  } else {
+    // Error handling
+  }
+}, []);
 
     const hashParams = new URLSearchParams(location.hash.split("?")[1]);
     const queryParams = new URLSearchParams(location.search);
